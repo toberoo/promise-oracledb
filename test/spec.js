301d654019ccf56f491bce075147ff05061a2260
Shehroze Khan <toberoo@gmail.com>
2016-03-28 11:28:16 -0400

    Added test runner: mocha

--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var db = require('promise-oracledb');
+var db = require(__dirname +'/../index.js');
 
 describe('Test Promise Oracle Database', function() {
 	/* Redacted, confidential information, come up with a way for others to
